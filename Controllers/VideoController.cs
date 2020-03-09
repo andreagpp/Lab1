55383b001728b79e77a478900508e9c2d2e416ae
Andrea Parga <andrea_parga01@hotmail.com>
2020-03-09 13:06:09 -0400

    Actualizacion

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using MVCPlantilla.Utilerias;
+
+namespace MvcPlantilla.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            //Consultar la informacion en la Base de datos//
+            ViewData["video"] = BaseHelper.ejecutarConsulta(
+                                                            "Select * from video",
+                                                            CommandType.Text);
+
+            return View();
+        }
+
+    }
+}
